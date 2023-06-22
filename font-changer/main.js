fc95180ff4c889fa718bdeb55309e0cb105eb208
Mouatez Benariba <benariba.mouatez@gmail.com>
2023-06-22 14:47:38 +0100

    make the challenge dynamic

--- font-changer/main.js
+++ font-changer/main.js
@@ -2,11 +2,5 @@ const font = document.getElementById('text-fonts');
 const text = document.querySelector('.font-changer__result');
 
 font.addEventListener('change', (e) => {
-  if (e.target.value === 'metalmania') {
-    text.style.fontFamily = `'Metal Mania', cursive`;
-  } else if (e.target.value === 'caesar') {
-    text.style.fontFamily = `'Caesar Dressing', cursive`;
-  } else if (e.target.value === 'opensans') {
-    text.style.fontFamily = `'Open Sans', sans-serif`;
-  }
+  text.style.fontFamily = e.target.value;
 });
